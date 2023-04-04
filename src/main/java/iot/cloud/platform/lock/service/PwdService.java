bdcaba59d73f6ae951f5652a4b43b5147e2e99a3
qiaozhididi <1343013603@qq.com>
2023-04-04 23:04:07 +0800

    20230404--Part6代码更新，6.3实验已完成

--- src/main/java/iot/cloud/platform/lock/service/PwdService.java
+++ src/main/java/iot/cloud/platform/lock/service/PwdService.java
@@ -1,18 +1,19 @@
 package iot.cloud.platform.lock.service;
 
-import iot.cloud.platform.lock.vo.ResMsg;
-
-import java.util.Date;
+import iot.cloud.platform.lock.entity.PwdEntity;
 
 public interface PwdService {
 
     /**
      * 检查密码是否正确
+     *
      * @param pwd
      * @return
      */
     boolean verifyPwd(String pwd);
 
+    boolean isValidPwd(String pwd);
+
     //TODO: 新增修改固定密码方法resetFixedPwd
-    boolean resetFixedPwd(String pwd);
+    PwdEntity resetFixedPwd(String oPwd,String nPwd);
 }
